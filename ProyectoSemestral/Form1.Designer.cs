12df1609eb842391335df400e60c8a08660053ca
Teñikaler Smith <tenikaler.smith@utp.ac.pa>
2020-10-27 12:24:34 -0500

    pequeños cambios en form1

--- ProyectoSemestral/Form1.Designer.cs
+++ ProyectoSemestral/Form1.Designer.cs
@@ -111,7 +111,7 @@
             // 
             this.groupBox1.Controls.Add(this.btnBorrar);
             this.groupBox1.Controls.Add(this.dataGridViewDetalle);
-            this.groupBox1.Location = new System.Drawing.Point(431, 44);
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(668, 442);
             this.groupBox1.TabIndex = 0;
@@ -184,7 +184,7 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Location = new System.Drawing.Point(0, 521);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1122, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(738, 22);
             this.statusStrip1.TabIndex = 4;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -558,7 +558,7 @@
             this.menuReportes});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1122, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(738, 28);
             this.menuStrip1.TabIndex = 10;
             this.menuStrip1.Text = "Archivo";
             // 
@@ -620,7 +620,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1122, 543);
+            this.ClientSize = new System.Drawing.Size(738, 543);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.groupBox1);
