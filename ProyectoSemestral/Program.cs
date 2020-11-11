30eba2118b77c47ca3ed885d64e2adb64a3a1ee7
Teñikaler Smith <tenikaler.smith@utp.ac.pa>
2020-11-11 02:18:35 -0500

    los cambios que hice en al nombre de form

--- ProyectoSemestral/Program.cs
+++ ProyectoSemestral/Program.cs
@@ -17,7 +17,7 @@ namespace ProyectoSemestral
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMDI());
         }
     }
 }
