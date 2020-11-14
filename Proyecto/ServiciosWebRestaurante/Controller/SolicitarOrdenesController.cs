9e6dc3bdc07a4cf32b463d5f4de1df2f715988bc
Hiram Rivas <hiram.rivas@utp.ac.pa>
2020-11-14 14:00:23 -0500

    modelados de las base de datos es solo un ejemplo, no encontre en sus ramas como las habian modelado en clases

---
+++ Proyecto/ServiciosWebRestaurante/Controller/SolicitarOrdenesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using clsNegocios;
+
+namespace ServiciosWebRestaurante.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitarOrdenesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("mesas")]
+        public IActionResult solicitarMesas()
+        {
+            clsMesas mesas = new clsMesas();
+            List<clsMesas> todos = new List<clsMesas>();
+            todos = mesas.buscaMesas();
+            if (todos.Count == 0)
+                return NotFound(todos);
+            else
+                return Ok(todos);
+        }
+    }
+}
---
+++ Proyecto/clsNegocios/clsOrdenEncabezado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace clsNegocios
+{
+    class clsOrdenEncabezado
+    {
+        public string id_orden { get; set; }
+        public string fecha_orden { get; set; }
+        public string total { get; set; }
+        public string id_usuario { get; set; }
+        public string numero_mesa { get; set; }
+        public string id_cliente { get; set; }
+        public string estado { get; set; }
+
+    }
+}
